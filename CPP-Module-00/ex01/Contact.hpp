71aac7abdef43bf1003a8c54dde3b9390329e3d6
motelti <mteltipr03@gmail.com>
2025-08-15 16:31:36 +0100

    added CPP contact and phonebook CLASSES

--- CPP-Module-00/ex01/Contact.hpp
+++ CPP-Module-00/ex01/Contact.hpp
@@ -6,7 +6,32 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 13:44:58 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/15 13:44:59 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/15 16:25:48 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+class Contact {
+	private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+	
+	public:
+    Contact();
+    void setContact(const std::string& fn, const std::string& ln, const std::string& nn,
+                    const std::string& pn, const std::string& ds);
+    std::string getField(int idx) const;
+    void printContact() const;
+    bool isEmpty() const;
+};
+
+#endif
--- CPP-Module-00/ex01/PhoneBook.hpp
+++ CPP-Module-00/ex01/PhoneBook.hpp
@@ -6,7 +6,28 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 13:45:02 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/15 13:45:03 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/15 16:25:37 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook {
+	private:
+	Contact contacts[8];
+	int contactCount;
+	int oldestIndex;
+	
+	public:
+	PhoneBook();
+	void addContact(const Contact& c);
+	void listContacts() const;
+	bool validIndex(int idx) const;
+	void showContact(int idx) const;
+	int getContactCount() const;
+};
+
+#endif
