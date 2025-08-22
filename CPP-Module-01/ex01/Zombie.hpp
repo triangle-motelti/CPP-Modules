97361c5918a2946f6d62bb4afa1eff251c20f94b
motelti <mteltipr03@gmail.com>
2025-08-22 11:50:32 +0100

    implemented the main, still need more testing overall good

--- CPP-Module-01/ex01/Zombie.hpp
+++ CPP-Module-01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 17:58:30 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 10:30:23 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/22 11:38:55 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,5 @@ public:
 	void setName(std::string name);
 	void announce(void);
 };
+
+Zombie* zombieHorde(int N, std::string name);
--- CPP-Module-01/ex01/main.cpp
+++ CPP-Module-01/ex01/main.cpp
@@ -6,8 +6,26 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 17:59:07 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 10:20:09 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/22 11:38:51 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+int main(void)
+{
+	Zombie	*horde;
+	int		N;
+	
+	N = 5;
+	horde = zombieHorde(N, "HordeMember");
+	if (!horde) {
+		std::cout << "No zombies created!" << std::endl;
+		return 1;
+	}
+	for (int i = 0; i < N; ++i)
+		horde[i].announce();
+	delete[] horde;
+
+	return 0;
+}
