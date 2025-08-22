703e74b0a339252e05079601bd8399327ad70d08
motelti <mteltipr03@gmail.com>
2025-08-22 10:34:43 +0100

    implemented Zombie main task

--- CPP-Module-01/ex01/Zombie.cpp
+++ CPP-Module-01/ex01/Zombie.cpp
@@ -6,8 +6,25 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 17:59:10 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 10:20:19 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/22 10:33:48 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie() : name("") {}
+
+Zombie::~Zombie()
+{
+	std::cout << name << ": is destroyed" << std::endl;
+}
+
+void Zombie::setName(std::string name)
+{
+	this->name = name;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
