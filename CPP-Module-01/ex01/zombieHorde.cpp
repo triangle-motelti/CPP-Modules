5b7eb8ca45378a312a1fa799abe3ee5945b930b2
motelti <mteltipr03@gmail.com>
2025-08-22 11:36:42 +0100

    implemented zombieHorde that allocate N zombie

--- CPP-Module-01/ex01/zombieHorde.cpp
+++ CPP-Module-01/ex01/zombieHorde.cpp
@@ -6,8 +6,20 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 18:29:17 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 10:20:24 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/22 11:35:13 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie *zombieHorde(int N, std::string name)
+{
+    Zombie *horde;
+    
+	if (N <= 0)
+		return NULL;
+	horde = new Zombie[N];
+	for (int i = 0; i < N; ++i)
+		horde[i].setName(name);
+	return horde;
+}
