08d0c54bceeadfd3320409cdf8f344ac0bbdf6e8
motelti <mteltipr03@gmail.com>
2025-08-22 13:59:12 +0100

    implemented the task of the ex02 that it is references

--- CPP-Module-01/ex02/main.cpp
+++ CPP-Module-01/ex02/main.cpp
@@ -6,7 +6,29 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 13:20:11 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 13:20:12 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/22 13:56:36 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str;
+	std::string *stringPTR;
+	std::string &stringREF = str;
+	
+	str = "HI THIS IS BRAIN";
+	stringPTR = &str;
+
+	std::cout << "Address of str:      " << &str << std::endl;
+	std::cout << "Address held by PTR: " << stringPTR << std::endl;
+	std::cout << "Address held by REF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str:        " << str << std::endl;
+	std::cout << "Value pointed by PTR:" << *stringPTR << std::endl;
+	std::cout << "Value pointed by REF:" << stringREF << std::endl;
+
+	return 0;
+}
