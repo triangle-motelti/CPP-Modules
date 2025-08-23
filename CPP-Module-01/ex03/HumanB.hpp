2ddc1b02b9d03622d32b19680bc265fd45c89ccf
motelti <mteltipr03@gmail.com>
2025-08-23 13:55:02 +0100

    new implementation of HumanB class

--- CPP-Module-01/ex03/HumanB.hpp
+++ CPP-Module-01/ex03/HumanB.hpp
@@ -6,7 +6,7 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:38 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/23 13:52:36 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/23 13:54:21 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,13 @@
 
 #include "Weapon.hpp"
 
+class HumanB
+{
+private:
+	std::string name;
+	Weapon* weapon;
+public:
+	HumanB(const std::string& name);
+	void setWeapon(Weapon& weapon);
+	void attack();
+};
