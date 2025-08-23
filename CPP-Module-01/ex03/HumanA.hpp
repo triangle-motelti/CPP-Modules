62c24b8d5261a98346ecd23a2be56708b0067d92
motelti <mteltipr03@gmail.com>
2025-08-23 13:16:15 +0100

    adding Makefile and implemented class HumanA And HumanB

--- CPP-Module-01/ex03/HumanA.hpp
+++ CPP-Module-01/ex03/HumanA.hpp
@@ -6,7 +6,19 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:34 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 18:35:35 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/23 12:06:35 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "Weapon.hpp"
+
+class HumanA {
+    private :
+    Weapon weapon;
+    std::string name;
+
+    public :
+    void attack(name, weapon);
+};
--- CPP-Module-01/ex03/HumanB.hpp
+++ CPP-Module-01/ex03/HumanB.hpp
@@ -6,7 +6,19 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:38 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 18:35:39 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/23 12:02:40 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "Weapon.hpp"
+
+class HumanB {
+    private :
+    Weapon weapon;
+    std::string name;
+
+    public :
+    void attack(name, Weapon);
+};
