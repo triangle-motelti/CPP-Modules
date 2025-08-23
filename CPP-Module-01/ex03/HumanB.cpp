29d937bc55d3728cbe81d0ef484939e8340e4c1b
motelti <mteltipr03@gmail.com>
2025-08-23 13:59:39 +0100

    implemented HumanB setWeapon and attack

--- CPP-Module-01/ex03/HumanB.cpp
+++ CPP-Module-01/ex03/HumanB.cpp
@@ -6,7 +6,23 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:35:36 by motelti           #+#    #+#             */
-/*   Updated: 2025/08/22 18:35:37 by motelti          ###   ########.fr       */
+/*   Updated: 2025/08/23 13:58:33 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanB.hpp"
+
+HumanB::HumanB(const std::string& name) : name(name), weapon(NULL) {}
+
+void HumanB::setWeapon(Weapon& weapon)
+{
+	this->weapon = &weapon;
+}
+
+void HumanB::attack()
+{
+	if (weapon)
+		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+	else
+		std::cout << name << " has no weapon to attack with" << std::endl;
+}
