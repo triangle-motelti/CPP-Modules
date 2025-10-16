7e5866fe44951f3e842d5d421622bb9f7379adb1
motelti-triangle <mteltipr03@gmail.com>
2025-10-16 18:51:18 +0100

    Harl 2.0 ex05

---
+++ CPP-Module-01/ex05/main.cpp
@@ -0,0 +1,23 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main() {
+    Harl harl;
+
+    std::cout << "--- DEBUG ---" << std::endl;
+    harl.complain("DEBUG");
+
+    std::cout << "--- INFO ---" << std::endl;
+    harl.complain("INFO");
+
+    std::cout << "--- WARNING ---" << std::endl;
+    harl.complain("WARNING");
+
+    std::cout << "--- ERROR ---" << std::endl;
+    harl.complain("ERROR");
+
+    std::cout << "--- UNKNOWN ---" << std::endl;
+    harl.complain("NOTALEVEL");
+
+    return 0;
+}
