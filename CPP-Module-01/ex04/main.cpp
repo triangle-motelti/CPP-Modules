149ae537b0d8503ca92fadb77f400be94b1f304a
motelti <mteltipr03@gmail.com>
2025-10-30 15:14:25 +0100

    fix a bug in the 'int main'

--- CPP-Module-01/ex04/main.cpp
+++ CPP-Module-01/ex04/main.cpp
@@ -6,37 +6,30 @@
 /*   By: motelti <motelti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 16:40:25 by motelti           #+#    #+#             */
-/*   Updated: 2025/10/17 16:40:26 by motelti          ###   ########.fr       */
+/*   Updated: 2025/10/30 15:03:25 by motelti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "Sed.hpp"
+#include <iostream>
 
-Sed::Sed(const std::string& filename, const std::string& s1, const std::string& s2)
-	: _filename(filename), _s1(s1), _s2(s2)
-{}
+#include "Sed.hpp"
+#include <iostream>
 
-bool Sed::process() {
-	std::ifstream infile(_filename.c_str());
-	if (!infile) {
-		std::cerr << "Error: could not open input file: " << _filename << std::endl;
-		return false;
-	}
-	std::ofstream outfile((_filename + ".replace").c_str());
-	if (!outfile) {
-		std::cerr << "Error: could not create output file: " << _filename << ".replace" << std::endl;
-		return false;
-	}
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
 
-	std::string line;
-	while (std::getline(infile, line)) {
-		size_t pos = 0;
-		while (_s1.length() > 0 && (pos = line.find(_s1, pos)) != std::string::npos) {
-			line = line.substr(0, pos) + _s2 + line.substr(pos + _s1.length());
-			pos += _s2.length();
-		}
-		outfile << line << std::endl;
-	}
-	return true;
+    Sed sed(argv[1], argv[2], argv[3]);
+    if (!sed.process())
+        return 1;
+
+    std::cout << "File processed successfully!" << std::endl;
+    return 0;
 }
+
